332e0405ce87c06eafbc26012d70af6d035abe96
giselurr <gisela.malmberg0007@stud.hkr.se>
2026-01-27 11:11:30 +0100

    sql

---
+++ sql/tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Users (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    username VARCHAR(50) NOT NULL,
+    password CHAR(60) NOT NULL
+);
+
+CREATE TABLE Room (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Device (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    id_room UUID NOT NULL REFERENCES Room(id),
+    quantity NUMERIC NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE Scale (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    id_device UUID NOT NULL REFERENCES Device(id),
+    value NUMERIC NOT NULL,
+    max_value NUMERIC NOT NULL
+);
