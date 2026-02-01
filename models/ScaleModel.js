5a465135d5cab0c3188e22328e333591739e936d
Pernilla Ahlström Wennerberg <pernilla.ahlstrom_wennerberg0018@stud.hkr.se>
2026-02-01 10:38:16 +0100

    Added docstrings and made SQL changes

--- models/ScaleModel.js
+++ models/ScaleModel.js
@@ -1,60 +1,86 @@
 //Imports goes here
 import dbs from '../services/DatabaseService.js'
 /**
- * 
- * A model to interact with the 'Scale' table in the database
- * 
- * @class
- */
+* Model for the scale
+* 
+*/
 class ScaleModel {
 
-    #table = 'Scale'
-
+    /** 
+    * Gets a value for that scale
+    * @param {string} id - UUID to identify the scale
+    * @return {Promise<{value: number, max_value: number}>} - returns value and max value for that scale
+    * @throws {Error} - if no scale with that id is found
+    */
+    
     async getValue(id){
-        let sql = 'GET * FROM ?? WHERE id = ??'
-        const args = [this.#table, id]
-        return await dbs.query(sql, args)
-    }
-
-    async setValue(id, value, max_value){
-        let sql = 'INSERT INTO ?? (id_device, value, max_value) VALUES (?, ?, ?)'
-        const args = [this.#table, id, value, max_value]
+        let sql = 'SELECT value, max_value FROM Scale WHERE id = $1'
+        const args = [id]
         const result = await dbs.query(sql, args)
-        return result.id
-
+        const row = result.rows[0]
+        if(!row){
+            throw new Error('No scale for this device found.')
+        }
+        return {value: row.value,
+                max_value: row.max_value
+        }
     }
+
+    /** 
+    * Sets a value for that scale
+    * @param {string} device_id - UUID of the device
+    * @param {number} value - the value that will be the current value
+    * @param {number} max_value - max value for that device
+    * @return {Promise<{id: string, value: number, max_value: number }>} - returns id, value and max value for that scale
+    * @throws {Error} - If it was not possible to set the value for that device at this time
+    */
+    
+    async setValue(device_id, value, max_value){
+        let sql = 'INSERT INTO Scale (id_device, value, max_value) VALUES ($1, $2, $3) RETURNING id, value, max_value'
+        const args = [device_id, value, max_value]
+        const result = await dbs.query(sql, args)
+        const row = result.rows[0]
+        if(!row){
+            throw new Error('Error setting the value at this time.')
+        }
+        return {id: row.id, 
+                value: row.value,
+                max_value: row.max_value
+        }
+    }
+
+    /** 
+    * Updates the value for that scale
+    * @param {string} id - UUID to identify the scale
+    * @param {number} value - the value that will be the current value
+    * @return {Promise<boolean>} - returns true if update was successfull
+    * @throws {Error} - If the value exceeds max_value
+    */
 
     async updateValue(id, value){
-        const scale = this.getValue(id)
+        const scale = await this.getValue(id)
         if (scale.max_value < value){
-            throw new Error ('Value can not exeed max value')
+            throw new Error ('Value can not exceed max value')
         }
-        const sql = 'UPDATE ?? SET value = ? WHERE id = ?'
-        const args = [this.#table, value, id]
+        const sql = 'UPDATE Scale SET value = $1 WHERE id = $2'
+        const args = [value, id]
         const result = await dbs.query(sql, args)
-        return result.affectedRows > 0
+        return result.rowCount > 0
     }
 
+    /** 
+    * Deletes the value for that scale
+    * @param {string} id - UUID to identify the scale
+    * @return {Promise<boolean>} - returns true if delete was successfull
+    */
+
     async deleteValue(id){
-        const sql = 'DETETE FROM ?? WHERE id =?'
-        const args = [this.#table, id]
+        const sql = 'DELETE FROM Scale WHERE id =$1'
+        const args = [id]
         const result = await dbs.query(sql, args)
-        return result.affectedRows > 0
+        return result.rowCount > 0
     }
 }
 
 
 export default new ScaleModel()
-
-
-/**
- * 
- * 
- * CREATE TABLE Scale (
-    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    id_device UUID NOT NULL REFERENCES Device(id),
-    value NUMERIC NOT NULL,
-    max_value NUMERIC NOT NULL
-);
-
- */
