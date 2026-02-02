3fbf709b8b40b36c37d03efd7f75ccf1e03208d3
Pernilla Ahlström Wennerberg <pernilla.ahlstrom_wennerberg0018@stud.hkr.se>
2026-02-02 20:22:59 +0100

    Basic RoomModel class

---
+++ services/DatabaseService.js
@@ -0,0 +1,27 @@
+//imports
+/**
+ * Database service for database operations
+ * 
+ * TODO!
+ * 
+ */
+class DatabaseService {
+
+    /**
+     * 
+     * Executes the query
+     * 
+     * TODO!
+     * 
+     */
+
+    async query(query, args = []){
+        const [rows] = ''
+        return rows
+    }
+
+
+}
+
+export default new DatabaseService()
+
