1d828094b397e65e38c9cd3593c494dfc8939b21
giselurr <gisela.malmberg0007@stud.hkr.se>
2026-02-26 18:09:32 +0100

    start on Websocket skeleton and made changes to handler

--- src/handler/WSHandler.js
+++ src/handler/WSHandler.js
@@ -1,140 +1,139 @@
-import DeviceModel from "../model/DeviceModel"
-import RoomModel from "../model/RoomModel"
+import DeviceModel from "../model/DeviceModel";
+import RoomModel from "../model/RoomModel";
 
-export class WSHandler{
-    
-    /** Option two for message handeling 
-     * @param {JSON} message the websocket message
-     */
-    messagehandler(message){
-        let handler = handlers(message.type)
-        handler(message.data)
+export class WSHandler {
+	/**call to setDevice in model
+	 * @param {JSON} data object payload from message
+	 * @throws {Error} If it was not able to call on setDevice function in DeviceModel
+	 */
+	async create_device(data) {
+		const id_room = data.id_room;
+		const ip = data.ip;
+		const device_name = data.name;
+		const description = data.description;
+		try {
+			await DeviceModel.setDevice(id_room, ip, device_name, description);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    }
+	/**call to setDevice in model
+	 * @param {JSON} data object payload from message
+	 * @throws {Error} If it was not able to call on setDevice function in DeviceModel
+	 */
+	async create_room(data) {
+		const room_name = data.name;
+		try {
+			await RoomModel.setRoom(room_name);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+	/**call to updateDevice name and description in model
+	 * @param {JSON} data object payload from message
+	 */
+	async update_device(data) {
+		const id = data.id;
+		const device_name = data.name;
+		const description = data.description;
+		try {
+			await DeviceModel.updateDevice(id, device_name, description);
+		} catch (e) {
+			console.error(e);
+		}
+	}
+	/**call to updateRoomwith new name in model
+	 * @param {JSON} data object payload from message
+	 */
+	async update_room(data) {
+		const id = data.id;
+		const room_name = data.name;
+		try {
+			await RoomModel.updateRoom(id, room_name);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    /**call to setDevice in model
-     * @param {JSON} data object payload from message
-     * @throws {Error} If it was not able to call on setDevice function in DeviceModel 
-     */
-    async creat_device(data){
-        id_room = data.id_room
-        ip = data.ip
-        device_name = data.name
-        description = data.description
-        try{
-            await DeviceModel.setDevice(id_room, ip, device, description)  
-        }catch(e){
-            console.log(e)
-        }
-        
-    }
-    /**call to setDevice in model
-     * @param {JSON} data object payload from message
-     * @throws {Error} If it was not able to call on setDevice function in DeviceModel 
-     */
-    async create_room(data){
-        room_name = data.name
-        try{
-            await RoomModel.setRoom(room_name)  
-        }catch(e){
-            console.log(e)
-        }
-    }
-    /**Update device name and description  
-     * @param {JSON} data object payload from message
-    */
-    async update_device(data){
-        id = data.id
-        device_name = data.name
-        description = data.description
-        try{
-            await DeviceModel.updateDevice(id, device_name, description)  
-        }catch(e){
-            console.log(e)
-        }
-    }
-    /**Update room name 
-     * @param {JSON} data object payload from message
-    */
-    async update_room(data){
-        id = data.id
-        room_name = data.name
-        try{
-            await RoomModel.updateRoom(id, room_name)  
-        }catch(e){
-            console.log(e)
-        }
-    }
+	/**call to deleteRoom in room model
+	 * @param {JSON} data object payload from message
+	 */
+	async delete_room(data) {
+		const id = data.id;
+		try {
+			await RoomModel.deleteRoom(id);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    /**Delete room
-     * @param {JSON} data object payload from message
-     */
-    async delete_room(data){
-        id = data.id
-        try{
-            await RoomModel.deleteRoom(id, room_name)  
-        }catch(e){
-            console.log(e)
-        }
-    }
+	/**call to deleteDevice in device model
+	 * @param {JSON} data object payload from message
+	 */
+	async delete_device(data) {
+		const id = data.id;
+		try {
+			await DeviceModel.deleteDevice(id);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-     /**Delete device
-     * @param {JSON} data object payload from message
-     */
-    async delete_device(data){
-        id = data.id
-        
-        try{
-            await DeviceModel.deleteDevice(id, room_name)  
-        }catch(e){
-            console.log(e)
-        }
-    }
+	/**call to updateValue in device model
+	 * @param {JSON} data object payload from message
+	 */
+	async update_value(data) {
+		const id = data.id;
+		const value = data.value;
+		try {
+			await DeviceModel.updateValue(id, value);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    /**Update scale value in device
-     * @param {JSON} data object payload from message
-     */
-    async update_value(data){
-        id = data.id
-        value = data.value
-        try{
-            await DeviceModel.updateValue(id, value)  
-        }catch(e){
-            console.log(e)
-        }
-    }
+	/**call to getDevices in device model
+	 */
+	async get_device() {
+		try {
+			await DeviceModel.getDevices();
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-    async get_device(data){
-        id = data.id
-        try{
-            await DeviceModel.getDevices(id)  
-        }catch(e){
-            console.log(e)
-        }
-    }
-
-    async get_room(data){
-        try{
-            await RoomModel.getRoom(data.id)
-        }catch(e){
-            console.log(e)
-        }
-    }
-
-
-
-    
-
+	/**call to getRoom in room model
+	 * @param {JSON} data object payload from message
+	 */
+	async get_room(data) {
+		try {
+			await RoomModel.getRoom(data.id);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 }
 
-/** Has all the functions related to type  
-*/
-const handlers = {
-    "create_room": create_room,
-    "creat_device": create_device,
-    "update_device": update_device,
-    "update_room": update_room,
-    "delete_room": delete_room,
-    "delete_device": delete_device,
-    "update_value": update_value
-    }
+const handler = new WSHandler();
+
+/**Message handeling
+ * @param {JSON} type the message type
+ * @param {JSON} payload the message payload
+ */
+export const messagehandler = async (type, payload) => {
+	const handlers = {
+		create_room: handler.create_room.bind(handler),
+		creat_device: handler.create_device.bind(handler),
+		update_device: handler.update_device.bind(handler),
+		update_room: handler.update_room.bind(handler),
+		delete_room: handler.delete_room.bind(handler),
+		delete_device: handler.delete_device.bind(handler),
+		update_value: handler.update_value.bind(handler),
+		get_devices: handler.get_device(),
+		get_room: handler.get_room.bind(handler),
+	};
+
+	const handelfunction = handlers[type];
+	await handelfunction(payload);
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hallo :)");
+import { WebSocketServer } from "ws";
+import { messagehandler } from "./handler/WSHandler";
+
+const websocket = new WebSocketServer({ port: 8080 });
+
+websocket.on("connection", function connection(ws) {
+	ws.on("message", function message(data) {
+		const mesg = JSON.parse(data);
+		messagehandler(mesg.prototype, mesg.payload);
+	});
+	ws.on("error", console.error);
+});
