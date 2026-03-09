ec240f0646bf92efc0a513a59eb2adba9686535c
PernillaAW <pernilla.ahlstrom_wennerberg0018@stud.hkr.se>
2026-03-09 10:25:05 +0100

    saved#

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hallo :)");
+import express from "express";
+import db from "../src/service/DatabaseService.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+async function dbTest() {
+	const result = await db.query("SELECT NOW()");
+	console.log("WORKS =>", result);
+}
+
+dbTest();
+
+(app.listen(3000),
+	() => {
+		console.log("Server running on port 3000...");
+	});
