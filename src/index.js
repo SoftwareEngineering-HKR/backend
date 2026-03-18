8590d1177085278eece218c6b637d1667cfbf6de
Nikolas <nikolas@a-schwarz.info>
2026-03-18 15:16:31 +0100

    added basic Bluetooth implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { NetworkDiscoveryService } from "./service/NetworkDiscoveryService.js";
 import { MqttBrokerService } from "./service/MqttBrokerService.js";
 import { WebSocketService } from "./service/WebSocketService.js";
+import { BluetoothService } from "./service/BluetoothService.js";
 
 NetworkDiscoveryService.startNetworkDiscovery();
 const broker = new MqttBrokerService();
 broker.start();
 const webSocketService = new WebSocketService();
 webSocketService.startWebSocket();
+
+const bluetoothService = new BluetoothService();
+try {
+	await bluetoothService.start();
+} catch (error) {
+	console.error(error);
+}
