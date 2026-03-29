89ee5066d9919574ed035ccc5536e9f5bf25dbbc
Pernilla Ahlström Wennerberg <pernilla.ahlstrom_wennerberg0018@stud.hkr.se>
2026-03-29 10:10:32 +0200

    added a addUserToDevice function

--- src/model/UserDevicesModel.js
+++ src/model/UserDevicesModel.js
@@ -27,6 +27,23 @@ class UserDeviceModel extends EventEmitter {
 	 * TODO: here a function should be implemented that is only for admins: add connections between devices and users
 	 * This should emit an event to which the Websocketservice must listen to and update its map of devices.
 	 */
+
+	/**
+	 * Connect users to devices they are allowed to control
+	 * @param {string} id_user - UUID to identify the user
+	 * @param {string} id_device - VARCHAR that identifies the device
+	 * @return {Promise<boolean>} - returns true if user was able to be connected to the device
+	 * @throws {Error} - throws error if the query fails
+	 */
+	async addUserToDevice(id_user, id_device) {
+		const sql = `INSERT INTO user_devices (id_user, id_device) VALUES ($1, $2)`;
+		const args = [id_user, id_device];
+		const result = await dbs.query(sql, args);
+		if (result.rowCount > 0) {
+			this.emit("addedUserToD", { id_user, id_device });
+		}
+		return result.rowCount > 0;
+	}
 }
 
 export default new UserDeviceModel();
